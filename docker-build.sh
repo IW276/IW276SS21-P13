83b2ff67e9c3d02c989c6c646ab75996999fd092
Lukas Seitz <lausedev@gmail.com>
2021-04-25 21:24:25 +0200

    Improved docker-compose.yml and docker-build.sh.

--- docker-build.sh
+++ docker-build.sh
@@ -5,7 +5,7 @@
 cp /etc/apt/sources.list.d/nvidia-l4t-apt-source.list .
 cp /etc/apt/trusted.gpg.d/jetson-ota-public.asc .
 
-docker image build -f Dockerfile -t p13-demo:1.0.0 .
+sudo docker image build -f Dockerfile -t asl-p13 .
 
 # clean up after ourselves
 rm nvidia-l4t-apt-source.list
