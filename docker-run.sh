4b747aadeeb78a1b31e487fc8a783851abcb241b
Lukas Seitz <lausedev@gmail.com>
2021-05-08 12:03:10 +0200

    Test display - 3.

--- docker-run.sh
+++ docker-run.sh
@@ -2,4 +2,4 @@
 
 CONTAINER="asl-p13"
 
-docker run $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && DISPLAY=:0.0 python3 demo.py"
+docker run $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && $DISPLAY && DISPLAY=:0.0 python3 demo.py"
