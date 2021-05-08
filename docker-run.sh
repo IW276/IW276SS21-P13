60434cff18ff9b09def8bc2ac860e855e94d6762
Lukas Seitz <lausedev@gmail.com>
2021-05-08 12:44:37 +0200

    Test display - 9.

--- docker-run.sh
+++ docker-run.sh
@@ -3,4 +3,4 @@
 CONTAINER="asl-p13"
 
 docker run --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-        $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && DISPLAY=:1.0 python3 datareadandshow.py"
+        $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && python3 datareadandshow.py"
