b33b93aec59328fc2cb4ad3633f79372d617cb14
Lukas Seitz <lausedev@gmail.com>
2021-05-08 16:28:36 +0200

    Added --net=host.

--- docker-run.sh
+++ docker-run.sh
@@ -2,6 +2,6 @@
 
 CONTAINER="asl-p13"
 
-docker run -v /tmp/.X11-unix:/tmp/.X11-unix \
+docker run --net=host -v /tmp/.X11-unix:/tmp/.X11-unix \
            -e DISPLAY=$DISPLAY \
            $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && python3 datareadandshow.py"
