9385eeaebea60e1192be12c5939e925bdf1c9b58
Lukas Seitz <lausedev@gmail.com>
2021-05-08 17:00:52 +0200

    Test display - 29.

--- docker-run.sh
+++ docker-run.sh
@@ -2,9 +2,14 @@
 
 CONTAINER="asl-p13"
 
-docker run --net=host -v /tmp/.X11-unix:/tmp/.X11-unix \
-           -e DISPLAY=$DISPLAY \
-           -v="~/.Xauthority" \
+docker run -it -d \
+           --net=host \
+           --env="DISPLAY" \
+           --env="QT_X11_NO_MITSHM=1" \
+           --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+           --device="/dev/video0:/dev/video0" \
+           --volume="/path/to/your/sharedDockerFiles:/root/sharedDockerFiles" \
+           --volume="/etc/machine-id:/etc/machine-id" \
            $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && python3 datareadandshow.py"
 export containerId=$(docker ps -l -q)
 
