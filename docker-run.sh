fca86ac061cae261bc1acaf6b068f68bb96639d8
Lukas Seitz <lausedev@gmail.com>
2021-05-08 16:49:01 +0200

    Test display - 23.

--- docker-run.sh
+++ docker-run.sh
@@ -4,4 +4,10 @@ CONTAINER="asl-p13"
 
 docker run --net=host -v /tmp/.X11-unix:/tmp/.X11-unix \
            -e DISPLAY=$DISPLAY \
+           --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+           --volume="~/.Xauthority" \
            $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && python3 datareadandshow.py"
+export containerId=$(docker ps -l -q)
+
+sudo xhost +local:`sudo docker inspect --format='{{ .Config.Hostname }}' $containerId`
+sudo docker start $containerId
