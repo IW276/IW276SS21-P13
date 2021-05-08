85853f97b18ed2f238fd057dcd790282d9dd9675
Lukas Seitz <lausedev@gmail.com>
2021-05-08 12:01:56 +0200

    Test display.

--- docker-run.sh
+++ docker-run.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# 1. argument: path to a video directory on the host.
-# 2. argument: filename of the video to be processed as found in the path specified by the first argument.
-#
-# Returns: the output video placed in the video directory specified above.
-
 CONTAINER="asl-p13"
 
-docker run --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-        $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && python3 demo.py"
+docker run $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && DISPLAY=:0.1 python3 demo.py"
