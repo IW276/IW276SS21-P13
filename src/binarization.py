1386a6035192e5437e13c5aea14f62d007ad231f
Lukas Seitz <lausedev@gmail.com>
2021-05-09 15:42:39 +0200

    Reformatted file.

--- src/binarization.py
+++ src/binarization.py
@@ -1,28 +1,21 @@
-import urllib.request
-import os
 import cv2
-import imutils
-import numpy as np
-import matplotlib.pyplot as plt
 
-#method calculates a binary image, with a given, fixed threshold value
+
+# method calculates a binary image, with a given, fixed threshold value
 def bina_fixed_threshold(image, threshold):
-    ret, thresh1 = cv2.threshold(image, threshold , 255, cv2.THRESH_BINARY)
+    ret, thresh1 = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY)
     return thresh1
 
-#adaptive calculation; threshold value is the mean of neighbourhood area
-#blocksize: decides the size of the neighbourhood area
+
+# adaptive calculation; threshold value is the mean of neighbourhood area
+# blocksize: decides the size of the neighbourhood area
 def bina_adapt_mean_threshold(image):
     blocksize = 11
-    thresh2 = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,blocksize,2)
+    thresh2 = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blocksize, 2)
     return thresh2
 
-#flag TRESH_OTSU is set, so the algorithm finds the optimal threshold value, which is given in ret
+
+# flag TRESH_OTSU is set, so the algorithm finds the optimal threshold value, which is given in ret
 def bina_otsu(image):
-    ret,tresh3 = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    ret, tresh3 = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return tresh3
-
-
-
-
-
