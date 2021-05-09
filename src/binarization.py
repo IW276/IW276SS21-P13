e0f7c89cc57f5928ee0e2b2293a95bb01003e2f8
bjrecht <johannesbrecht92@gmail.com>
2021-05-09 11:20:23 +0200

    binarization (fixed threshold and mean area)

---
+++ src/binarization.py
@@ -0,0 +1,24 @@
+import urllib.request
+import os
+import cv2
+import imutils
+import numpy as np
+import matplotlib.pyplot as plt
+
+#method calculates a binary image, with a given, fixed threshold value
+def bina_fixed_threshold(image, threshold):
+    ret, thresh1 = cv2.threshold(image, threshold , 255, cv2.THRESH_BINARY)
+    return thresh1
+
+#adaptive calculation; threshold value is the mean of neighbourhood area
+#blocksize: decides the size of the neighbourhood area
+def bina_adapt_mean_threshold(image):
+    blocksize = 11
+    thresh2 = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,blocksize,2)
+    return thresh2
+
+
+
+
+
+
