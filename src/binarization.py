4c1effb42df285750cf9f2dc5d3b213827cf660d
bjrecht <johannesbrecht92@gmail.com>
2021-05-09 14:47:01 +0200

    binarization (Otsu added)

--- src/binarization.py
+++ src/binarization.py
@@ -17,6 +17,10 @@ def bina_adapt_mean_threshold(image):
     thresh2 = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,blocksize,2)
     return thresh2
 
+#flag TRESH_OTSU is set, so the algorithm finds the optimal threshold value, which is given in ret
+def bina_otsu(image):
+    ret,tresh3 = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return tresh3
 
 
 
