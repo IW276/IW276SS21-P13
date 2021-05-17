e2f1b3e76e28f94a2a3dad6e3238a83036ab2a09
Lukas Seitz <lausedev@gmail.com>
2021-05-17 23:47:09 +0200

    Improved docker-run.sh.

--- docker-run.sh
+++ docker-run.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-export DISPLAY=:1
-
 CONTAINER="asl-p13"
 
-docker run -v /tmp/.X11-unix:/tmp/.X11-unix \
-           --net=host \
-           -e DISPLAY=$DISPLAY \
-           $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && python3 demo.py --datasets_path $1"
+docker run -v $1:/datasets $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && python3 demo.py --datasets_path /datasets/"
 
 echo "Resulting images can be found in $1pipeline-results"
-
