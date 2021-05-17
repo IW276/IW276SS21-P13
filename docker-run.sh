be038165aacd23c1b4513c20da5b236be23bb71d
Lukas Seitz <lausedev@gmail.com>
2021-05-17 22:48:20 +0200

    Adapted docker-run.sh with command lineargument with datasets path.

--- docker-run.sh
+++ docker-run.sh
@@ -5,9 +5,9 @@ export DISPLAY=:1
 CONTAINER="asl-p13"
 
 docker run -v /tmp/.X11-unix:/tmp/.X11-unix \
-           $1:/datasets\
            --net=host \
            -e DISPLAY=$DISPLAY \
-           $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && python3 demo.py --datasets_path /datasets/"
+           $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && python3 demo.py --datasets_path $1"
 
-echo "Resulting images can be found in $1/pipeline-results"
+echo "Resulting images can be found in $1pipeline-results"
+
