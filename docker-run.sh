a493c00f6854d822f709f7d407103cfa1a1efe27
Lukas Seitz <lausedev@gmail.com>
2021-05-18 00:17:46 +0200

    Fixed wrong result directory.

--- docker-run.sh
+++ docker-run.sh
@@ -2,8 +2,8 @@
 
 CONTAINER="asl-p13"
 
-docker run -v $1:/datasets -v $2:/pipelineresults \
+docker run -v $1:/datasets -v $2:/pipeline_results \
         $CONTAINER /bin/bash -c \
         "cd IW276SS21-P13/src && python3 demo.py --datasets_path /datasets/ --pipeline_results /pipeline_results/"
 
-echo "Resulting images can be found in $1pipeline-results"
+echo "Resulting images can be found in $2pipeline-results"
