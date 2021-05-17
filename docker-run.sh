9e51804068720af2d4d8fd97098a9f1af8bab36d
Lukas Seitz <lausedev@gmail.com>
2021-05-18 00:14:00 +0200

    Added missing volume commandline parameter.

--- docker-run.sh
+++ docker-run.sh
@@ -2,6 +2,8 @@
 
 CONTAINER="asl-p13"
 
-docker run -v $1:/datasets $2:/pipeline_results $CONTAINER /bin/bash -c "cd IW276SS21-P13/src && python3 demo.py --datasets_path /datasets/ --pipeline_results /pipeline_results/"
+docker run -v $1:/datasets -v $2:/pipelineresults \
+        $CONTAINER /bin/bash -c \
+        "cd IW276SS21-P13/src && python3 demo.py --datasets_path /datasets/ --pipeline_results /pipeline_results/"
 
 echo "Resulting images can be found in $1pipeline-results"
