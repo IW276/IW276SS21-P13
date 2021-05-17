c3306dfda0f1eb7171029ca91eeaf946c769fa9c
Lukas Seitz <lausedev@gmail.com>
2021-05-17 22:35:02 +0200

    Using absolute path of images which are read from command line.

--- src/demo.py
+++ src/demo.py
@@ -1,41 +1,38 @@
 import argparse
+import os
 
 import cv2
 import numpy as np
 
 from pipeline import Pipeline
 
-parser = argparse.ArgumentParser(description='Automatic white and confession balancing')
-parser.add_argument('--datasets_path', type=str)
+parser = argparse.ArgumentParser(description='Automatic white balancing and exposure compensation.')
+parser.add_argument('--datasets-path', type=str)
 args = parser.parse_args()
 
-image_names_file = args.datasets_path + "RTTS light/ImageSets/test.txt"
-image_results_directory = args.datasets_path + "pipeline-results"
+datasets_path = args.datasets_path
 
-pipeline = Pipeline(image_names_file, image_results_directory)
+image_names_file = os.path.join(datasets_path, "RTTS light", "ImageSets", "test.txt")
+image_results_directory = os.path.join(datasets_path, "pipeline-results")
+
+pipeline = Pipeline(datasets_path, image_names_file, image_results_directory)
 pipeline.run()
 
-datei = open('../datasets/RTTS light/ImageSets/test.txt', 'r')
+datei = open(image_names_file, 'r')
 print("Datei opened")
 
 for zeile in datei:
-    picture_name = "../datasets/RTTS light/JPEGImages/" + zeile[:-1] + ".png"
-    picture_name_result = "../datasets/pipeline-results/" + zeile[:-1] + ".png"
-    # TODO: Remove or comment print calls after testing
-    # print(picture_name)
+    picture_name = os.path.join(datasets_path, "RTTS light", "JPEGImages", zeile[:-1] + ".png")
+    picture_name_result = os.path.join(datasets_path, "pipeline-results", zeile[:-1] + ".png")
 
-    width = 600
-    height = 400
-    dim = (width, height)
+    dim = (600, 400)
 
     img = cv2.imread(picture_name)
     img = cv2.resize(img, dim)
     img_result = cv2.imread(picture_name_result)
-    # img_result = cv2.resize(img_result, dim)
+    img_result = cv2.resize(img_result, dim)
     resultImage = np.concatenate((img, img_result), axis=1)
     height, width = img.shape[:2]
-    # TODO: Remove or comment print calls after testing
-    # print("width: ", width, "height: ", height)
     cv2.imshow('image', resultImage)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+
+import os
 from datetime import datetime
 
 import cv2
@@ -9,36 +11,35 @@ from gamma_correction import adjust_gamma
 from smoothing import Filter
 
 
-def read_image(zeile):
-    picture_name = "../datasets/RTTS light/JPEGImages/" + zeile[:-1] + ".png"
-    # print(picture_name)
-    return cv2.imread(picture_name)
-
-
-def write_result_image(self, zeile, result_img):
-    cv2.imwrite(self.pipeline_result_directory + "/" + zeile[:-1] + ".png", result_img)
-
-
 class Pipeline:
-    def __init__(self, image_names_file, image_results_directory):
+    def __init__(self, datasets_path, image_names_file, image_results_directory):
         self.start_time = None
         self.end_time = None
         self.fps = None
         self.img_count = 0
+        self.datasets_path = datasets_path
         self.image_names_file = open(image_names_file, "r")
         self.pipeline_result_directory = image_results_directory
+
+    def read_image(self, zeile):
+        picture_name = os.path.join(self.datasets_path, "RTTS light", "JPEGImages", zeile[:-1] + ".png")
+        return cv2.imread(str(picture_name))
+
+    def write_result_image(self, zeile, result_img):
+        picture_name = os.path.join(self.pipeline_result_directory, zeile[:-1] + ".png")
+        cv2.imwrite(picture_name, result_img)
 
     def run(self):
         self.start_time = datetime.now().timestamp()
         print("Current Time =", self.start_time)
         for zeile in self.image_names_file:
             self.img_count += 1
-            img = read_image(zeile)
+            img = self.read_image(zeile)
             image_setup = evaluate(img)
             result_img = adjust_gamma(img, image_setup.average)
             result_img = Filter(result_img).gaussian_filter()
             result_img = bina_adapt_mean_threshold(cv2.cvtColor(result_img, cv2.COLOR_BGR2GRAY))
-            write_result_image(self, zeile, result_img)
+            self.write_result_image(zeile, result_img)
 
         self.end_time = datetime.now().timestamp()
         self.fps = self.img_count / (self.end_time - self.start_time)
