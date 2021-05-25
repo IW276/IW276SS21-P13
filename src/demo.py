9f36a20a820d91f8582339351b99b42dc16ee989
Lukas Seitz <lukas.s.studiert@gmail.com>
2021-05-25 15:35:11 +0200

    Improved demo.

--- src/demo.py
+++ src/demo.py
@@ -1,41 +1,48 @@
 import argparse
 import os
-
 import cv2
 import numpy as np
-
 from pipeline import Pipeline
 
-parser = argparse.ArgumentParser(description='Automatic white balancing and exposure compensation.')
-parser.add_argument('--datasets_path', type=str)
-parser.add_argument('--pipeline_results', type=str)
-args = parser.parse_args()
 
-datasets_path = args.datasets_path
-image_results_directory = args.pipeline_results
+def extract_command_line_arguments():
+    parser = argparse.ArgumentParser(description='Automatic white balancing and exposure compensation.')
+    parser.add_argument('--datasets_path', type=str)
+    parser.add_argument('--pipeline_results', type=str)
+    args = parser.parse_args()
+    return args.datasets_path, args.pipeline_results
 
-image_names_file = os.path.join(datasets_path, "RTTS_light", "ImageSets", "test.txt")
 
-pipeline = Pipeline(datasets_path, image_names_file, image_results_directory)
+def extract_image_names():
+    image_names_file_name = os.path.join(datasets_path, "RTTS_light", "ImageSets", "test.txt")
+    print("Using pictures defined in file ", image_names_file_name)
+
+    image_names_file = open(image_names_file_name, 'r')
+    result = [name for name in image_names_file if not name[:-1] == ""]
+    image_names_file.close()
+    return result
+
+
+def show_results():
+    for image_name in image_names:
+        picture_name = os.path.join(datasets_path, "RTTS_light", "JPEGImages", image_name[:-1] + ".png")
+        picture_name_result = os.path.join(result_directory, image_name[:-1] + ".png")
+        dim = (600, 400)
+        img = cv2.imread(picture_name)
+        img = cv2.resize(img, dim)
+        img_result = cv2.imread(picture_name_result)
+        img_result = cv2.resize(img_result, dim)
+        result_image = np.concatenate((img, img_result), axis=1)
+        cv2.imshow('image', result_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+datasets_path, result_directory = extract_command_line_arguments()
+
+image_names = extract_image_names()
+
+pipeline = Pipeline(datasets_path, image_names, result_directory)
 pipeline.run()
 
-datei = open(image_names_file, 'r')
-print("Datei opened")
-
-for zeile in datei:
-    picture_name = os.path.join(datasets_path, "RTTS_light", "JPEGImages", zeile[:-1] + ".png")
-    picture_name_result = os.path.join(image_results_directory, zeile[:-1] + ".png")
-
-    dim = (600, 400)
-
-    img = cv2.imread(picture_name)
-    img = cv2.resize(img, dim)
-    img_result = cv2.imread(picture_name_result)
-    img_result = cv2.resize(img_result, dim)
-    resultImage = np.concatenate((img, img_result), axis=1)
-    height, width = img.shape[:2]
-    cv2.imshow('image', resultImage)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-datei.close()
-print("datei closed!")
+show_results()
--- src/pipeline.py
+++ src/pipeline.py
@@ -12,37 +12,36 @@ from smoothing import Filter
 
 
 class Pipeline:
-    def __init__(self, datasets_path, image_names_file, image_results_directory):
+    def __init__(self, datasets_path, image_names, image_results_directory):
         self.start_time = None
         self.end_time = None
         self.fps = None
         self.img_count = 0
         self.datasets_path = datasets_path
-        self.image_names_file = open(image_names_file, "r")
+        self.image_names = image_names
         self.pipeline_result_directory = image_results_directory
 
-    def read_image(self, zeile):
-        picture_name = os.path.join(self.datasets_path, "RTTS_light", "JPEGImages", zeile[:-1] + ".png")
+    def read_image(self, image_name):
+        picture_name = os.path.join(self.datasets_path, "RTTS_light", "JPEGImages", image_name[:-1] + ".png")
         return cv2.imread(str(picture_name))
 
-    def write_result_image(self, zeile, result_img):
-        picture_name = os.path.join(self.pipeline_result_directory, zeile[:-1] + ".png")
+    def write_result_image(self, image_name, result_img):
+        picture_name = os.path.join(self.pipeline_result_directory, image_name[:-1] + ".png")
         cv2.imwrite(picture_name, result_img)
 
     def run(self):
+        print("Executing automatic white balancing and exposure compensation ...", end=" ")
         self.start_time = datetime.now().timestamp()
-        print("Current Time =", self.start_time)
-        for zeile in self.image_names_file:
-            if zeile[:-1] == "":
-                break
+        for image_name in self.image_names:
             self.img_count += 1
-            img = self.read_image(zeile)
+            img = self.read_image(image_name)
             image_setup = evaluate(img)
             result_img = adjust_gamma(img, image_setup.average)
             result_img = Filter(result_img).gaussian_filter()
             result_img = bina_adapt_mean_threshold(cv2.cvtColor(result_img, cv2.COLOR_BGR2GRAY))
-            self.write_result_image(zeile, result_img)
+            self.write_result_image(image_name, result_img)
 
         self.end_time = datetime.now().timestamp()
         self.fps = self.img_count / (self.end_time - self.start_time)
-        print("Frames per Second = ", self.fps)
+        print("done.")
+        print("Execution speed: ", int(self.fps), "frames per second")
